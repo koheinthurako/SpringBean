6f1ef5f75c87635cf54e1c71be8b1f2f2b19f360
koheinthurako <captainthura99@gmail.com>
2024-01-19 22:49:20 +0630

    noted about spring beans

--- src/main/java/com/ioc/Demo/com/ioc/Demo/DemoApplication.java
+++ src/main/java/com/ioc/Demo/com/ioc/Demo/DemoApplication.java
@@ -9,6 +9,14 @@ import org.springframework.context.annotation.Configuration;
 // Spring အကြောင်းစမယ်
 // 1. IoC => ဘယ်ဟာတွေနဲ့ ဘယ်လိုတွေချိတ်ပြီးအလုပ်လုပ်ကြမလဲဆိုတဲ့ concept
 // 2. Dependency Injection => အမှန်တကယ်ချိတ်ဆက်ပေးသည့် function
+/*
+ 3. Spring Container and Their Types
+	 Spring Program ကို စ run လိုက်တာနဲ့ spring server ထဲမှာ ကိုယ်ဆောက်ထားတဲ့ project တွေကိုထားတဲ့ container နှစ်နေရာရှိတယ်
+	 အဲ့တာက BeanFactory and ApplicationContext 
+	 - BeanFactory ကတော့ application စ run တဲ့အချိန်မှာ Object တွေတစ်ခါတည်းမဆောက်ပေးပဲနဲ့
+	 object တစ်ခုကိုဆောက်ချင်ပါတယ်ဆိုပြီး request လုပ်မှ BeanFactory က ဆောက်ပေးတယ်
+	 - ApplicationContext ကတော့ application စလိုက်တာနဲ့ ဆောက်ချင်တဲ့ object တွေကို တစ်ခါတည်းဆောက်ပေးစေချင်ရင်သုံးတယ်
+*/
 
 @SpringBootApplication
 public class DemoApplication {
@@ -16,13 +24,15 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 //		Bean = Object
+//		Spring မှာ object ဆောက်မယ်ဆိုရင် ရိုးရိုး java တုန်းကလို အများကြီး ရေးနေစရာမလိုပဲ အကုန်လုံးကိုဆောက်ပစ်နိုင်တယ်
 //		Object အားလုံးဆောက်တဲ့နေရာမှာသုံးတဲ့ Spring function => ApplicationContext ရဲ့ (new AnnotationConfigApplicationContext)
 //		AnnotationConfigApplicationContext() parameter ထဲမှာက package name ကိုမှန်အောင်ထည့်ပေးရမယ်
 //		သူက အဲ့ဒီ package အောက်မှာရှိတဲ့ class တွေအားလုံးကို object ဆောက်ပေးနိုင်တယ် ဆောက်တဲ့အခါမှာတော့ @Configuration ကိုသုံးထားတဲ့
-//		class တွေကိုပဲ object ဆောက်ခွင့်ပေးတယ် object ဆောက်တော့မယ်ဆိုရင်တော့ သူ့ထဲက getBean() method ကိုအောက်ကလိုသုံးပေးရတယ်
+//		class တွေအကုန်လုံးက အောက်က context ထဲကိုဝင်သွားတယ် ဝင်တဲ့အခါ default အနေနဲ့ class name တွေက အစစာလုံးအသေးတွေပြောင်းသွားတယ်
+//		ကိုယ်ဆောက်ချင်တဲ့ class ကို object ဆောက်တော့မယ်ဆိုရင်တော့ context.getBean() method ကိုအောက်ကလိုသုံးပေးရတယ်
 		ApplicationContext context = new AnnotationConfigApplicationContext("com.ioc.Demo.com.ioc.Demo");
-		
-//		getBean ကတော့ ဆောက်ထားတဲ့ object တွေအားလုံးထဲက တစ်ခုကိုဆွဲထုတ်ပြီး type casting ပြောင်း object လုပ်
+//		getBean ကတော့ @Configuration သုံးထားတဲ့ class တွေကို ဆွဲထုတ်ပြီး object ဆောက်တဲ့နေရာမှာသုံးတယ်
+//		return type က ရိုးရိုး object ဆိုတော့ သက်ဆိုင်တဲ့ class object ဖြစ်အောင် type casting လုပ်ပြီး အောက်ကလိုပြန်ပြောင်းပေးရတယ်
 //		Time24 obj = (Time24) context.getBean("time24"); 
 //		obj.showTime();
 		
--- src/main/java/com/ioc/Demo/com/ioc/Demo/Time12.java
+++ src/main/java/com/ioc/Demo/com/ioc/Demo/Time12.java
@@ -2,6 +2,8 @@ package com.ioc.Demo.com.ioc.Demo;
 
 import org.springframework.context.annotation.Configuration;
 
+//	Spring မှာ Configuration keyword ထည့်မှ object ဆောက်ပေးနိုင်တယ်
+//	တကယ်က Configuration က Component ကနေအမွေဆက်ခံထားတာဖြစ်တယ် တကယ် object ဆောက်ပေးတဲ့ function က component
 @Configuration
 public class Time12 implements TimeFormat {
 
